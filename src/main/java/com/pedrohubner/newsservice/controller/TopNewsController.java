90ec9aede2a09d773a5741fdc04d1267546cb1b5
pedrohubner <pedronhubner@gmail.com>
2022-11-19 09:25:04 -0300

    First commit.

---
+++ src/main/java/com/pedrohubner/newsservice/controller/TopNewsController.java
@@ -0,0 +1,7 @@
+package com.pedrohubner.newsservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/v1/home")
+public class TopNewsController {
+}
---
+++ src/test/java/com/pedrohubner/newsservice/NewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrohubner.newsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
